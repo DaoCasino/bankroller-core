adda4469f47d5aa9afd5e12a7443f9f5ea5be261
Alex Step <kellas@MBP-Aleksandr.lan>
2018-02-20 12:30:31 +0100

    rm log

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import path  from 'path'
 import DApps from 'dapps/dapps.js'
 
 const rollbar_path = path.resolve('../../tools/rollbar/index.js')
-console.log(rollbar_path)
 if (fs.existsSync(rollbar_path)) {
   require(rollbar_path)
 }
