2681fe8f1a2a74433016a74b2f78323c4300ad4d
Alex Step <kellas@MacBook-Pro-Aleksandr.local>
2018-02-20 15:18:25 +0100

    update rollbar

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import DApps from 'dapps/dapps.js'
 
 const rollbar_path = path.resolve('../../tools/rollbar/index.js')
 if (fs.existsSync(rollbar_path)) {
-  require(rollbar_path)
+  require(rollbar_path)()
 }
 
 DApps.start()
