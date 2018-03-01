1442fea8e944b698f0ecea536dbcc23042e6fdb0
Ilya Shvyrialkin <ilyashvy@gmail.com>
2018-03-01 18:36:04 +0200

    add debug log

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ const path    = require('path')
 
 module.exports = {
   wallet_pass : '1234',
+  loglevel: 'none',
 
-  dapps_dir: path.join(path.resolve(),'./data/dapps/'),
+  dapps_dir: path.join(path.resolve(), './data/dapps/'),
   network:ropsten
 }
