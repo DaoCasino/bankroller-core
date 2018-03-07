24e899c9862f9448bcd3dc0f9f9dfc213d9e8d11
Alex Step <alex.step.kellas@gmail.com>
2018-03-07 13:55:21 +0200

    Add start message, console.log

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ import fs    from 'fs'
 import path  from 'path'
 import DApps from 'dapps/dapps.js'
 
+console.log('')
+console.log('')
+console.log('-------------------------------')
+console.log('BANKROLLER NODE START')
+console.log('process.env.DC_NETWORK: ', process.env.DC_NETWORK)
+console.log('-------------------------------')
+console.log('')
+console.log('')
+
 const rollbar_path = path.resolve('../../tools/rollbar/index.js')
 if (fs.existsSync(rollbar_path)) {
   require(rollbar_path)()
