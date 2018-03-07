e7ec51962ec201bb79f9474fc027c69851d1e61f
Alex Step <alex.step.kellas@gmail.com>
2018-03-07 16:17:49 +0200

    Add config for local testrpc network

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
-const ropsten = require('./configs/ropsten/config.js')
-const path    = require('path')
+const path = require('path')
+
+let network
+if (process.env.DC_NETWORK==='local') {
+	network = require('./configs/local/config.js')
+} else {
+	network = require('./configs/ropsten/config.js')
+}
+
 
 module.exports = {
   wallet_pass : '1234',
   loglevel: 'light',
 
   dapps_dir: path.join(path.resolve(), './data/dapps/'),
-  network:ropsten
+  network: network
 }
---
+++ configs/local/config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+const filpath = path.resolve('../protocol/build/contracts_adressess.json')
+if (!fs.existsSync(filpath)) {
+	console.log('')
+	console.log('')
+	console.log('Cant find contracts_adressess ', filpath)
+	console.log('')
+	console.log('BANKROLLER NODE SHUT DOWN')
+	console.log('')
+	proccess.exit()
+}
+
+const conf = require(filpath)
+
+
+// {
+// 	address:
+// 	abi:
+// }
+
+module.exports = {
+  upd     : '07.03.2018',
+  name    : 'local',
+  rpc_url : 'https://localhost:9545/',
+
+  contracts: {
+    erc20      : require('./contracts/erc20.js'),
+    paychannel : require('./contracts/paychannel.js')
+  },
+
+  gasPrice : 40 * 1000000000,
+  gasLimit : 40 * 100000
+}
