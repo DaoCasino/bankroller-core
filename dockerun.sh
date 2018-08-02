98bf175fd6aee03d2cecc4dc78c481fe84ed0c81
AlexStep <to@alexstep.com>
2018-08-02 19:09:39 +0300

    Optimize configs for SDK

---
+++ dockerun.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+DC_NETWORK=sdk            \
+DATA_PATH=data_sdk        \
+DATA_SUBPATH=sdk          \
+DAPPS_PATH=data_sdk/dapps \
+nodemon --watch ./ --exec babel-node ./server.js
