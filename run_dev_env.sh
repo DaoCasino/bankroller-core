657f6506756c37ad2b2d87fc02bb0f8513b3394e
Ilya fanyShu <ilyashvy@gmail.com>
2018-09-08 19:52:20 +0300

    fix balance check in eth module

--- run_dev_env.sh
+++ run_dev_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DC_NETWORK=local          \
-DATA_PATH=data_local      \
+DATA_PATH=data            \
 DATA_SUBPATH=local        \
 DAPPS_PATH=data/dapps     \
 nodemon --watch ./ --exec babel-node ./server.js
