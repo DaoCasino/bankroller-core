a376e077f54824664e026292dc7125596b309774
Ilya fanyShu <ilyashvy@gmail.com>
2018-09-12 14:57:27 +0300

    fix nodemon

--- run_dev_env.sh
+++ run_dev_env.sh
@@ -4,4 +4,5 @@ DC_NETWORK=local          \
 DATA_PATH=data            \
 DATA_SUBPATH=local        \
 DAPPS_PATH=data/dapps     \
-nodemon --watch ./ --exec babel-node ./server.js
+WATH_PATH=./              \
+npm run nodemon
