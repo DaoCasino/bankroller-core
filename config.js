78a5f01355a453ba9bb079b856a8fb93e0722543
AlexStep <to@alexstep.com>
2018-09-14 00:00:54 +0300

    remove unused functions

--- config.js
+++ config.js
@@ -4,17 +4,10 @@ let faucet_server = {}
 let network
 if (process.env.DC_NETWORK === 'sdk') {
   network = require('./configs/sdk/config.js')
-  faucet_server.get_acc_url = false
 } else if (process.env.DC_NETWORK === 'local') {
   network = require('./configs/local/config.js')
-  faucet_server.get_acc_url = 'http://localhost:8181/?get=account'
 } else {
   network = require('./configs/ropsten/config.js')
-  faucet_server.get_acc_url = 'https://stage.dao.casino/faucet/?get=account'
-  
-  if(process.env.NODE_ENV === 'production') {
-    faucet_server.get_acc_url = 'https://faucet.dao.casino/?get=account'
-  }
 }
 
 
@@ -24,8 +17,6 @@ module.exports = {
   privateKey : process.env.privateKey || false, 
   wallet_pass : '1234',
   loglevel: 'light',
-
-  faucet : faucet_server,
 
   dapps_dir    : path.join(path.resolve(), ( process.env.DAPPS_PATH || (data_path + '/dapps/') )),
   network      : network
