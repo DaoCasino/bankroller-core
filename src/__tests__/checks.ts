a577ca1ee16f05873e1f4ce4d528a678e5b3a1bf
alexstep <alex.step.kellas@gmail.com>
2018-10-11 19:13:39 +0300

    Merge

--- src/__tests__/checks.ts
+++ src/__tests__/checks.ts
@@ -1,12 +1,12 @@
-import { Eth } from 'dc-ethereum-utils';
-import _config from '../../config';
+import { Eth } from 'dc-ethereum-utils'
+import _config from '../../config'
 
-const { gasPrice: price, gasLimit: limit } = _config.network;
+const { gasPrice: price, gasLimit: limit } = _config.network
 const eth = new Eth({
-  httpProviderUrl: _config.network.rpc_url,
-  ERC20ContractInfo: _config.network.contracts.erc20,
-  gasParams: { price, limit },
-  privateKey: _config.privateKey
-});
+  privateKey        : _config.privateKey,
+  httpProviderUrl   : _config.network.rpc_url,
+  ERC20ContractInfo : _config.network.contracts.erc20,
+  gasParams         : { price, limit }
+})
 
-eth.initAccount();
+eth.initAccount()
