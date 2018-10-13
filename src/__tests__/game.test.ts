7c7b9e21f28a00a2fea933d0d7d6b60d14b163e5
Michael Oznobkin <michaeloznobkin@Michaels-MacBook-Pro-2.local>
2018-10-13 21:07:46 +0300

    feature: platformId for bankroller

--- src/__tests__/game.test.ts
+++ src/__tests__/game.test.ts
@@ -1,14 +1,14 @@
-import { IpfsTransportProvider, DirectTransportProvider } from 'dc-messaging'
-import { config } from 'dc-configs'
-import { Eth as Ethereum } from 'dc-ethereum-utils'
+import { IpfsTransportProvider, DirectTransportProvider } from "dc-messaging"
+import { config } from "dc-configs"
+import { Eth as Ethereum } from "dc-ethereum-utils"
 
-import { GlobalGameLogicStore, DApp } from 'dc-core'
-import Bankroller from '../dapps/Bankroller'
-import { Logger } from 'dc-logging'
-import { loadLogic } from '../dapps/FileUtils'
+import { GlobalGameLogicStore, DApp } from "dc-core"
+import Bankroller from "../dapps/Bankroller"
+import { Logger } from "dc-logging"
+import { loadLogic } from "../dapps/FileUtils"
 
+const logger = new Logger("test1")
 
-const logger = new Logger('test1')
 const directTransportProvider = new DirectTransportProvider()
 const startBankroller = async () => {
   try {
@@ -24,41 +24,47 @@ const startGame = async () => {
     const gameTransportProvider = directTransportProvider // await IpfsTransportProvider.createAdditional()
 
     // ropsten env
-    let manifestFile = config.DAppsPath + '/FTE1/dapp.manifest.js'
-    let privkey = '0x6A5AE922FDE5C8EE877E9470F45B8030F60C19038E9116DB8B343782D9593602'
+    let manifestFile = config.DAppsPath + "/FTE1/dapp.manifest.js"
+    let privkey =
+      "0x6A5AE922FDE5C8EE877E9470F45B8030F60C19038E9116DB8B343782D9593602"
     // local env
-    if (process.env.DC_NETWORK === 'local') {
-      manifestFile = config.DAppsPath + '/ex1/dapp.manifest.js'
-      privkey = '0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f'
+    if (process.env.DC_NETWORK === "local") {
+      manifestFile = config.DAppsPath + "/ex1/dapp.manifest.js"
+      privkey =
+        "0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"
     }
 
-    const dappManifest = require( manifestFile )
+    const dappManifest = require(manifestFile)
 
     const {
       gasPrice: price,
       gasLimit: limit,
       web3HttpProviderUrl: httpProviderUrl,
       contracts,
+      platformId
     } = config
     const Eth = new Ethereum({
       httpProviderUrl,
       ERC20ContractInfo: contracts.ERC20,
       gasParams: { price, limit },
-      privateKey: privkey,
+      privateKey: privkey
     })
 
     // Game loaded to store during bankroller start
-    const gameLogicFunction = new GlobalGameLogicStore().getGameLogic(dappManifest.slug)
+    const gameLogicFunction = new GlobalGameLogicStore().getGameLogic(
+      dappManifest.slug
+    )
     const dappParams = {
-      slug         : dappManifest.slug,
-      contract     : dappManifest.contract,
-      rules        : dappManifest.rules,
-      roomProvider : gameTransportProvider,
+      slug: dappManifest.slug,
+      platformId,
+      contract: dappManifest.contract,
+      rules: dappManifest.rules,
+      roomProvider: gameTransportProvider,
       gameLogicFunction,
       Eth
     }
     await Eth.initAccount()
-    const dapp         = new DApp(dappParams)
+    const dapp = new DApp(dappParams)
     const dappInstance = await dapp.startClient()
     return { game: dappInstance, Eth }
   } catch (error) {
@@ -72,31 +78,31 @@ const test1 = async () => {
   const showFunc = (source, data) => {
     logger.debug(`${source} ${new Date().toString()} ${JSON.stringify(data)}`)
   }
-  game.onPeerEvent('info', data => showFunc('Bankroller', data))
-  game.on('info', data => showFunc('Client', data))
+  game.onPeerEvent("info", data => showFunc("Bankroller", data))
+  game.on("info", data => showFunc("Client", data))
   await game.openChannel({
     playerAddress: Eth.getAccount().address,
     playerDeposit: 3,
-    gameData: [0, 0],
+    gameData: [0, 0]
   })
 
-  logger.info('Channel opened!')
+  logger.info("Channel opened!")
 
   const result1 = await game.callPeerGame({
     userBet: 1,
-    gameData: [1],
+    gameData: [1]
   })
   const result2 = await game.callPeerGame({
     userBet: 1,
-    gameData: [2],
+    gameData: [2]
   })
   const result3 = await game.callPeerGame({
     userBet: 1,
-    gameData: [3],
+    gameData: [3]
   })
 
-  logger.info('Start close channel')
-  
+  logger.info("Start close channel")
+
   game.closeChannel()
 }
 test1()
--- src/dapps/Bankroller.ts
+++ src/dapps/Bankroller.ts
@@ -1,71 +1,80 @@
-import { config } from 'dc-configs'
-import fs   from 'fs'
-import path from 'path'
-import { DApp, GlobalGameLogicStore } from 'dc-core'
-import { IpfsTransportProvider, IMessagingProvider } from 'dc-messaging'
-import { Eth }    from 'dc-ethereum-utils'
-import { Logger } from 'dc-logging'
+import { config } from "dc-configs"
+import fs from "fs"
+import path from "path"
+import { DApp, GlobalGameLogicStore } from "dc-core"
+import { IpfsTransportProvider, IMessagingProvider } from "dc-messaging"
+import { Eth } from "dc-ethereum-utils"
+import { Logger } from "dc-logging"
 import {
   getSubDirectoriee,
   loadLogic,
   saveFilesToNewDir,
-  removeDir,
-} from './FileUtils'
+  removeDir
+} from "./FileUtils"
 
-import { IBankroller, GameInstanceInfo } from '../intefaces/IBankroller'
+import { IBankroller, GameInstanceInfo } from "../intefaces/IBankroller"
 /*
  * Lib constructor
  */
-
-const logger = new Logger('Bankroller')
-const addressPrefix = 'Bankroller'
+const SERVER_APPROVE_AMOUNT = 100000000
+const logger = new Logger("Bankroller")
 
 export default class Bankroller implements IBankroller {
   private _started: boolean
   private _loadedDirectories: Set<string>
   private _eth: Eth
+  private _apiRoomAddress
+  private _platformId
+  private _blockchain
   gamesMap: Map<string, DApp>
   id: string
   private _transportProvider: IMessagingProvider
   constructor() {
     const {
+      platformId,
       gasPrice: price,
       gasLimit: limit,
       web3HttpProviderUrl: httpProviderUrl,
       contracts,
       privateKey,
+      blockchain
     } = config
-
+    this._platformId = platformId
+    this._blockchain = blockchain
     this._eth = new Eth({
       privateKey,
       httpProviderUrl,
       ERC20ContractInfo: contracts.ERC20,
-      gasParams: { price, limit },
+      gasParams: { price, limit }
     })
 
-    this.gamesMap           = new Map()
+    this.gamesMap = new Map()
     this._loadedDirectories = new Set()
-    this.tryLoadDApp        = this.tryLoadDApp.bind(this)
-    ;(global as any).DCLib  = new GlobalGameLogicStore()
+    this.tryLoadDApp = this.tryLoadDApp.bind(this)
+    ;(global as any).DCLib = new GlobalGameLogicStore()
   }
-
+  getApiRoomAddress(ethAddress: string) {
+    return `${this._platformId}_${this._blockchain}_${ethAddress}`
+  }
   async start(transportProvider: IMessagingProvider) {
     if (this._started) {
-      throw new Error('Bankroller allready started')
+      throw new Error("Bankroller allready started")
     }
     this._transportProvider = transportProvider
     await this._eth.initAccount()
-     
-    transportProvider.exposeSevice(
-      this._eth.getAccount().address.toLowerCase(),
-      this,
-      true
-    )
+    const ethAddress = this._eth.getAccount().address.toLowerCase()
+    await this._eth.ERC20ApproveSafe(ethAddress, SERVER_APPROVE_AMOUNT)
+    logger.debug(`ERC20 approved for ${ethAddress}`)
+
+    this._apiRoomAddress = this.getApiRoomAddress(ethAddress)
+
+    transportProvider.exposeSevice(this._apiRoomAddress, this, true)
     this._started = true
     const loadDirPromises = getSubDirectoriee(config.DAppsPath).map(
       this.tryLoadDApp
     )
     await Promise.all(loadDirPromises)
+    logger.info(`Bankroller started. Api address: ${this._apiRoomAddress}`)
     return this
   }
 
@@ -103,12 +112,13 @@ export default class Bankroller implements IBankroller {
       if (gameLogicFunction) {
         const { slug, rules, contract } = manifest
         const dapp = new DApp({
+          platformId: this._platformId,
           slug,
           rules,
           contract,
           roomProvider,
           gameLogicFunction,
-          Eth: this._eth,
+          Eth: this._eth
         })
         await dapp.startServer()
         this.gamesMap.set(slug, dapp)
