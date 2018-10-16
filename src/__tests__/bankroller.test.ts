bdedac06b78ca4cbbde1bd9a6f5163f1e9b4a544
Michael Oznobkin <michaeloznobkin@Michaels-MacBook-Pro-2.local>
2018-10-16 17:21:45 +0300

    feta: DAppFactory and mocha

--- src/__tests__/bankroller.test.ts
+++ src/__tests__/bankroller.test.ts
@@ -1,1 +1,7 @@
-import Bankroller from '../dapps/Bankroller'
+import Bankroller from "../dapps/Bankroller"
+import { describe, it, Test } from "mocha"
+import { expect } from "chai"
+
+const suite = describe("Bankroller Tests", () => {
+  it("game with remote bankroller", async () => {})
+})
