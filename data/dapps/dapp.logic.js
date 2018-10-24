7b0f694c2ee9bcfd6c632e523705a0b689e3beea
Your Name <you@example.com>
2018-10-24 10:30:33 +0300

    init commit

---
+++ data/dapps/dapp.logic.js
@@ -0,0 +1,20 @@
+/* global DCLib */
+DCLib.defineDAppLogic('Game_EX_v1', function () {
+  return {
+    play: function (userBet, gameData, randoms) {
+      const userNum   = gameData[0]
+      const randomNum = randoms[0]
+
+
+      let profit = -userBet
+
+      // if user win
+      if (userNum * 1 === randomNum * 1) {
+        profit = userBet * 2 - userBet
+      }
+
+      // return player profit
+      return profit
+    }
+  }
+})
