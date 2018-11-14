5f43c6ba3f1fca7fa0b8675ed2a50bd059ccd4c2
Gar|k <garik.djan@gmail.com>
2018-11-14 14:18:56 +0300

    add full test

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import Bankroller from './dapps/Bankroller'
 import { TransportProviderFactory } from 'dc-messaging'
 import { Logger } from 'dc-logging'
+import { config, TransportType } from 'dc-configs'
 
 const logger = new Logger('Bankroller:')
+
+let transportType: TransportType = config.default.transport
+if (process.env.DC_TRANSPORT && process.env.DC_TRANSPORT in TransportType) {
+  transportType = TransportType[process.env.DC_TRANSPORT]
+}
 
 logger.debug('')
 logger.debug('')
 logger.debug('-------------------------------')
 logger.debug('BANKROLLER NODE START          ')
+logger.debug('Bankroller transport:', transportType)
 logger.debug('process.env.DC_NETWORK: ', process.env.DC_NETWORK)
+
 logger.debug('Bankroller private key', process.env.ACCOUNT_PRIVATE_KEY)
 logger.debug('DApps path', process.env.DAPPS_FULL_PATH || process.env.DAPPS_PATH)
 logger.debug('-------------------------------')
@@ -17,7 +25,7 @@ logger.debug('')
 
 const startBankroller = async () => {
   try {
-    const factory = new TransportProviderFactory()
+    const factory = new TransportProviderFactory(transportType)
     const bankrollerTransportProvider = await factory.create()
     await new Bankroller().start(bankrollerTransportProvider)
   } catch (error) {
